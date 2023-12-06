ec3f280bb83f47d91ad521f9d2b5cff1a9eea3e2
zeqotibo <thibaultderouin5@gmail.com>
2023-12-06 15:21:28 +0100

    feat: add script monster

---
+++ Assets/Thibault/Script/Monster.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Monster : MonoBehaviour
+{
+   private void OnTriggerEnter(Collider collision)
+         {
+             if (collision.gameObject.CompareTag("Player"))
+             {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+             }
+   }
+}
